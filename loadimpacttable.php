9b317a6026a22c993ec6b9ff7dc5702897aaf706
ITS <its@irbbarcelona.org>
2017-03-29 17:17:02 +0200

    Uploaded IF

--- loadimpacttable.php
+++ loadimpacttable.php
@@ -28,8 +28,22 @@ $comm_params_array = explode(",", $conn_params);
 //mysql_select_db("dev_joomlaatirb");
 	
 // operational db
-$db = mysql_connect("irbsvr83.irb.pcb.ub.es", $comm_params_array[2], $comm_params_array[3]) or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db("sciprod");
+/*
+$db = mysqli_connect("irbsvr83.irb.pcb.ub.es", $comm_params_array[2], $comm_params_array[3], "sciprod");
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+*/
+
+define("DB_SERVER", "complete");
+define("DB_USERNAME", "complete");
+define("DB_PASSWORD", "complete");
+define("DB_DATABASE", "complete");
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
 
 ?>
 <html>
@@ -46,16 +60,16 @@ mysql_select_db("sciprod");
 $query = "SELECT *"
 . " FROM jos_sci_upload_impact_factor"
 ;
-$result = mysql_query($query);
-while ($row = mysql_fetch_assoc($result))
+$result = mysqli_query($db, $query);
+while ($row = mysqli_fetch_assoc($result))
 {
 	$journal = ucwords(strtolower(trim($row['journal'])));
 	
 	// miro si existe en journals
 	$query2 = "SELECT * from jos_sci_journals WHERE `description` = '" . $journal . "'";
-	$result2 = mysql_query($query2);
+	$result2 = mysqli_query($db, $query2);
 	
-	if (!mysql_num_rows($result2))
+	if (!mysqli_num_rows($result2))
 	{		
 		// insert new journal
 		$query3 = "INSERT INTO jos_sci_journals(`description`, `short_description`)"
@@ -63,12 +77,13 @@ while ($row = mysql_fetch_assoc($result))
 		;
 		//echo $query3 . '<br>';
 				
-		$result3 = mysql_query($query3);
+		$result3 = mysqli_query($db, $query3);
 		if (!$result3) {
-			die('Invalid query: ' . mysql_error());
+			die('Invalid query: ' . mysqli_error($db));
 		}
 		
 		echo 'He insertado el journal ' . $journal . ' en la tabla maestra.<br>';
+		
 	}
 }
 echo "End";
@@ -82,18 +97,18 @@ $query = "SELECT *"
 . " FROM jos_sci_journals"
 . " ORDER BY short_description"
 ;
-$result = mysql_query($query);
+$result = mysqli_query($db, $query);
 $i = 1; // order
-while ($row = mysql_fetch_assoc($result))
+while ($row = mysqli_fetch_assoc($result))
 {
 	$query2 = "UPDATE `jos_sci_journals`"
 	. " SET `order` = " . $i
 	. " WHERE `short_description` = '" . $row['short_description'] . "'"
 	;
 	//echo $query2 . '<br>';
-	$result2 = mysql_query($query2);
+	$result2 = mysqli_query($db, $query2);
 	if (!$result2) {
-	    die('Invalid query: ' . mysql_error());
+	    die('Invalid query: ' . mysqli_error($db));
 	}
 	
 	$i++;
@@ -110,8 +125,8 @@ echo "End";
 $query = "SELECT *"
 . " FROM jos_sci_journals"
 ;
-$result = mysql_query($query);
-while ($row = mysql_fetch_assoc($result))
+$result = mysqli_query($db, $query);
+while ($row = mysqli_fetch_assoc($result))
 {
 	echo "description ->" . $row['description'] . "<-, short_description ->" . $row['short_description'] . "<-<br>";
 }
@@ -124,13 +139,13 @@ echo "End";
  * Cargando impact factors
  */
 /*
-$my_year = '2014';
+$my_year = '2018';
 
 $query = "SELECT *"
 . " FROM jos_sci_upload_impact_factor"
 ;
-$result = mysql_query($query);
-while ($row = mysql_fetch_assoc($result))
+$result = mysqli_query($db, $query);
+while ($row = mysqli_fetch_assoc($result))
 {	
 	$journal = ucwords(strtolower(trim($row['journal'])));
 	$journal = str_replace(".", "", $journal);
@@ -138,35 +153,56 @@ while ($row = mysql_fetch_assoc($result))
 	// miro si existe en journals
 	$query2 = "SELECT * from jos_sci_journals WHERE `description` = '" . $journal . "'";
 	//echo $query2 . '<br>';
-	$result2 = mysql_query($query2);
-	$row2 = mysql_fetch_object($result2);
+	$result2 = mysqli_query($db, $query2);
+	$row2 = mysqli_fetch_object($result2);
 
-	if (mysql_num_rows($result2))
+	if (mysqli_num_rows($result2))
 	{
+	
+
+		$query9 = "UPDATE `jos_sci_journals` SET `tobedeleted` = '0'"
+		. " WHERE `id` = '". $row2->id . "'"
+		;
+		//echo $query9 . '<br>';
+		//break;
+		$result9 = mysqli_query($db, $query9);
+		if (!$result9) {
+			echo "A revisar: " . $query9 . "<br>";
+		}
+
+
 		$query4 = "SELECT * FROM jos_sci_journal_impact_factors WHERE `journal_id` = ". $row2->id . " AND `year` = '".$my_year."'";
-		$result4 = mysql_query($query4);
-		if (!mysql_num_rows($result4))
+		$result4 = mysqli_query($db, $query4);
+		if (!mysqli_num_rows($result4))
 		{
 			// inserto nuevo impact factor
-			$query3 = "INSERT INTO jos_sci_journal_impact_factors(`journal_id`, `year`, `impact_factor`)"
-			. " VALUES (" . $row2->id . ", '".$my_year."', " . $row['impact_factor'] . ")"
+			$query3 = "INSERT INTO jos_sci_journal_impact_factors(`journal_id`, `year`, `impact_factor`, `quartile`, `jcrd`, `sjrif`, `sjrq`, `sjrd`, `tobedeleted`)"
+			. " VALUES (" . $row2->id . ", '".$my_year."', " 
+			. ($row['jcrif'] == ''? 0:$row['jcrif']) .", '". $row['jcrq'] ."', '". $row['jcrd'] ."', "
+			. ($row['sjrif'] == ''? 0:$row['sjrif']) .", '". $row['sjrq'] ."', '". $row['sjrd'] ."', '0')"
 			;
-			echo $query3 . '<br>';
+			//echo $query3 . '<br>';
 			//break;
-			$result3 = mysql_query($query3);
+			$result3 = mysqli_query($db, $query3);
 			if (!$result3) {
 				echo "A revisar: " . $query3 . "<br>";
 			}
 		} else {
 			
 			// update nuevo impact factor
-			$query3 = "UPDATE `jos_sci_journal_impact_factors` SET `impact_factor` = '"
-			. $row['impact_factor'] . "'"
+			$query3 = "UPDATE `jos_sci_journal_impact_factors` SET"
+			." `impact_factor` = '" . ($row['jcrif'] == ''? 0:$row['jcrif']) . "'"
+			.", `quartile` = '" . $row['jcrq'] . "'"
+			.", `jcrd` = '" . $row['jcrd'] . "'"
+			.", `sjrif` = '" . ($row['sjrif'] == ''? 0:$row['sjrif']) . "'"
+			.", `sjrq` = '" . $row['sjrq'] . "'"
+			.", `sjrd` = '" . $row['sjrd'] . "'"
+			.", `tobedeleted` = '0'"
 			. " WHERE `journal_id` = '". $row2->id . "' AND `year` = '".$my_year."'"
 			;
-			echo $query3 . '<br>';
+			//echo $query3 . '<br>';
 			//break;
-			$result3 = mysql_query($query3);
+			$result3 = mysqli_query($db, $query3);
 			if (!$result3) {
 				echo "A revisar: " . $query3 . "<br>";
 			}
